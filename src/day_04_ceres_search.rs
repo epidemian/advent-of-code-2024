ec9ceefb07504fca8c270a4347d74ffa1465a77a
Demian Ferreiro <epidemian@gmail.com>
2024-12-29 12:39:06 -0300

    Day 4: improve implementation for both parts
    Simplify code in general and add some robustness test.

--- src/day_04_ceres_search.rs
+++ src/day_04_ceres_search.rs
@@ -1,44 +1,47 @@
 use itertools::iproduct;
+use std::array;
 
 pub fn run(input: &str) -> aoc::Result<String> {
     let (grid, w, h) = aoc::parse_char_grid(input)?;
-    let xmas_count: u64 = iproduct!(0..w, 0..h)
+
+    let xmas_count: usize = iproduct!(0..w, 0..h)
         .map(|(x, y)| {
+            #[rustfmt::skip]
             let dirs = [
-                [(0, 0), (1, 0), (2, 0), (3, 0)],
-                [(0, 0), (0, 1), (0, 2), (0, 3)],
-                [(0, 0), (1, 1), (2, 2), (3, 3)],
-                [(3, 0), (2, 1), (1, 2), (0, 3)],
+                // Right, left, down, up.
+                (1, 0), (-1, 0), (0, 1), (0, -1),
+                // Diagonals.
+                (1, 1), (-1, -1), (1, -1), (-1, 1)
             ];
-            let mut count = 0;
-            for d in dirs {
-                let chars = d.map(|(dx, dy)| grid.get(y + dy)?.get(x + dx).copied());
-                if chars == [Some('X'), Some('M'), Some('A'), Some('S')] {
-                    count += 1;
-                }
-                if chars == [Some('S'), Some('A'), Some('M'), Some('X')] {
-                    count += 1;
-                }
-            }
-            count
+            dirs.into_iter()
+                .filter(|(dx, dy)| {
+                    let strip = array::from_fn(|i| {
+                        let sx = x.wrapping_add_signed(dx * i as isize);
+                        let sy = y.wrapping_add_signed(dy * i as isize);
+                        grid.get(sy)?.get(sx).copied()
+                    });
+                    strip == [Some('X'), Some('M'), Some('A'), Some('S')]
+                })
+                .count()
         })
         .sum();
 
-    let x_mas_count = iproduct!(0..w - 2, 0..h - 2)
+    let x_mas_count = iproduct!(0..w.saturating_sub(2), 0..h.saturating_sub(2))
         .filter(|(x, y)| {
-            let cross = [(0, 0), (1, 1), (2, 2), (0, 2), (2, 0)];
-            let cross_chars = cross.map(|(dx, dy)| grid[y + dy][x + dx]);
-            matches!(
-                cross_chars,
-                ['M', 'A', 'S', 'M', 'S']
-                    | ['M', 'A', 'S', 'S', 'M']
-                    | ['S', 'A', 'M', 'M', 'S']
-                    | ['S', 'A', 'M', 'S', 'M']
-            )
+            let [center, top_l, top_r, bottom_r, bottom_l] =
+                [(1, 1), (0, 0), (0, 2), (2, 2), (2, 0)].map(|(dx, dy)| grid[y + dy][x + dx]);
+            center == 'A'
+                && matches!((top_l, bottom_r), ('M', 'S') | ('S', 'M'))
+                && matches!((top_r, bottom_l), ('M', 'S') | ('S', 'M'))
         })
         .count();
 
     Ok(format!("{xmas_count} {x_mas_count}"))
+}
+
+#[test]
+fn empty() {
+    assert_eq!(run("").unwrap(), "0 0")
 }
 
 #[test]
