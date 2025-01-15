578f001ae65e9ea9683d7c6f67079ef1589290ab
Demian Ferreiro <epidemian@gmail.com>
2025-01-15 18:29:10 -0300

    Day 8: make slight improvements

--- src/day_08_resonant_collinearity.rs
+++ src/day_08_resonant_collinearity.rs
@@ -1,14 +1,15 @@
-use itertools::{iproduct, Itertools};
+use itertools::{chain, iproduct, Itertools};
 
 pub fn run(input: &str) -> aoc::Result<String> {
     let (map, w, h) = aoc::parse_char_grid(input)?;
+    let is_inside_map = |&(x, y): &_| 0 <= x && x < w as i64 && 0 <= y && y < h as i64;
 
-    let antennas = iproduct!(0..w, 0..h)
+    let antenna_groups = iproduct!(0..w, 0..h)
         .map(|(x, y)| (map[y][x], (x as i64, y as i64)))
         .filter(|&(ch, _)| ch != '.')
         .into_group_map();
 
-    let antinode_count_p1 = antennas
+    let antinode_count_p1 = antenna_groups
         .values()
         .flat_map(|antennas| {
             antennas
@@ -19,11 +20,11 @@ pub fn run(input: &str) -> aoc::Result<String> {
                     [(x1 + dx, y1 + dy), (x2 - dx, y2 - dy)]
                 })
         })
-        .filter(|&(x, y)| 0 <= x && x < w as i64 && 0 <= y && y < h as i64)
+        .filter(is_inside_map)
         .unique()
         .count();
 
-    let antinode_count_p2 = antennas
+    let antinode_count_p2 = antenna_groups
         .values()
         .flat_map(|antennas| {
             antennas
@@ -31,13 +32,14 @@ pub fn run(input: &str) -> aoc::Result<String> {
                 .tuple_combinations()
                 .flat_map(|((x1, y1), (x2, y2))| {
                     let (dx, dy) = (x1 - x2, y1 - y2);
-                    let antinode_line = (0..)
-                        .map(move |i| (x1 + dx * i, y1 + dy * i))
-                        .take_while(|&(x, y)| 0 <= x && x < w as i64 && 0 <= y && y < h as i64);
-                    let antinode_opposite_line = (1..)
-                        .map(move |i| (x1 - dx * i, y1 - dy * i))
-                        .take_while(|&(x, y)| 0 <= x && x < w as i64 && 0 <= y && y < h as i64);
-                    antinode_line.chain(antinode_opposite_line)
+                    chain(
+                        (0..)
+                            .map(move |i| (x1 + dx * i, y1 + dy * i))
+                            .take_while(is_inside_map),
+                        (1..)
+                            .map(move |i| (x1 - dx * i, y1 - dy * i))
+                            .take_while(is_inside_map),
+                    )
                 })
         })
         .unique()
