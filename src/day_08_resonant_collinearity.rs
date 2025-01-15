4ae9d96fecb8d8dd08f123ccdeeae44da870e364
Demian Ferreiro <epidemian@gmail.com>
2025-01-15 19:09:31 -0300

    Day 8: minor improvements

--- src/day_08_resonant_collinearity.rs
+++ src/day_08_resonant_collinearity.rs
@@ -1,48 +1,46 @@
 use itertools::{chain, iproduct, Itertools};
-use std::collections::HashMap;
 
 pub fn run(input: &str) -> aoc::Result<String> {
     let (map, w, h) = aoc::parse_char_grid(input)?;
-    let is_inside_map = |&(x, y): &_| 0 <= x && x < w as i64 && 0 <= y && y < h as i64;
+    let is_inside_map = |(x, y)| 0 <= x && x < w as i64 && 0 <= y && y < h as i64;
 
-    let antenna_groups = iproduct!(0..w, 0..h)
+    let antennas_by_freq = iproduct!(0..w, 0..h)
         .map(|(x, y)| (map[y][x], (x as i64, y as i64)))
         .filter(|&(ch, _)| ch != '.')
         .into_group_map();
+    let antenna_groups = antennas_by_freq.into_values().collect_vec();
 
-    let antinode_count_p1 = count_unique_antinodes(&antenna_groups, |(x1, y1), (x2, y2)| {
+    let antinode_count_p1 = count_antinodes(&antenna_groups, |(x1, y1), (x2, y2)| {
         let (dx, dy) = (x1 - x2, y1 - y2);
         [(x1 + dx, y1 + dy), (x2 - dx, y2 - dy)]
             .into_iter()
-            .filter(is_inside_map)
+            .filter(|&p| is_inside_map(p))
     });
 
-    let antinode_count_p2 = count_unique_antinodes(&antenna_groups, |(x1, y1), (x2, y2)| {
+    let antinode_count_p2 = count_antinodes(&antenna_groups, |(x1, y1), (x2, y2)| {
         let (dx, dy) = (x1 - x2, y1 - y2);
         chain(
             (0..)
                 .map(move |i| (x1 + dx * i, y1 + dy * i))
-                .take_while(is_inside_map),
+                .take_while(|&p| is_inside_map(p)),
             (1..)
                 .map(move |i| (x1 - dx * i, y1 - dy * i))
-                .take_while(is_inside_map),
+                .take_while(|&p| is_inside_map(p)),
         )
     });
 
     Ok(format!("{antinode_count_p1} {antinode_count_p2}"))
 }
 
-type Point = (i64, i64);
-
-fn count_unique_antinodes<I>(
-    antenna_groups: &HashMap<char, Vec<(i64, i64)>>,
-    get_antinodes: impl Fn(Point, Point) -> I,
+fn count_antinodes<I>(
+    antenna_groups: &[Vec<(i64, i64)>],
+    get_antinodes: impl Fn((i64, i64), (i64, i64)) -> I,
 ) -> usize
 where
-    I: Iterator<Item = Point>,
+    I: Iterator<Item = (i64, i64)>,
 {
     antenna_groups
-        .values()
+        .iter()
         .flat_map(|antennas| {
             antennas
                 .iter()
