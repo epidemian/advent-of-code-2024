6a3f8c9878065d4b2a83c64aa22062bf9217fc9f
Demian Ferreiro <epidemian@gmail.com>
2025-01-21 01:17:42 -0300

    Day 9: use i32s numbers instead of Option<usize>
    This cuts the runtime from ~2.2s to ~1s.

--- src/day_09_disk_fragmenter.rs
+++ src/day_09_disk_fragmenter.rs
@@ -3,9 +3,9 @@ use std::iter::repeat_n;
 
 pub fn run(input: &str) -> aoc::Result<String> {
     let mut blocks = vec![];
-    for (i, ch) in input.trim().chars().enumerate() {
+    for (ch, i) in input.trim().chars().zip(0..) {
         let size = ch.to_digit(10).context("Expected a digit")?;
-        let block = if i % 2 == 0 { Some(i / 2) } else { None };
+        let block = if i % 2 == 0 { i / 2 } else { FREE };
         blocks.extend(repeat_n(block, size as usize));
     }
 
@@ -15,17 +15,19 @@ pub fn run(input: &str) -> aoc::Result<String> {
     Ok(format!("{p1_checksum} {p2_checksum}"))
 }
 
-fn compact_blocks(mut blocks: Vec<Option<usize>>) -> usize {
+const FREE: i32 = -1;
+
+fn compact_blocks(mut blocks: Vec<i32>) -> u64 {
     let mut free_idx = 0;
     loop {
-        while free_idx < blocks.len() && blocks[free_idx].is_some() {
+        while free_idx < blocks.len() && blocks[free_idx] != FREE {
             free_idx += 1;
         }
         if free_idx == blocks.len() {
             break;
         }
         let mut last_block = blocks.pop().unwrap();
-        while last_block.is_none() {
+        while last_block == FREE {
             last_block = blocks.pop().unwrap()
         }
         blocks[free_idx] = last_block;
@@ -33,24 +35,24 @@ fn compact_blocks(mut blocks: Vec<Option<usize>>) -> usize {
     checksum(&blocks)
 }
 
-fn compact_whole_files(mut blocks: Vec<Option<usize>>) -> usize {
-    let mut file_id = blocks.last().unwrap().unwrap();
+fn compact_whole_files(mut blocks: Vec<i32>) -> u64 {
+    let mut file_id = *blocks.last().unwrap();
     while file_id > 0 {
-        let file_idx = blocks.iter().position(|&b| b == Some(file_id)).unwrap();
+        let file_idx = blocks.iter().position(|&b| b == file_id).unwrap();
         let file_size = blocks[file_idx..]
             .iter()
-            .take_while(|&&b| b == Some(file_id))
+            .take_while(|&&b| b == file_id)
             .count();
         let free_chunk = blocks[0..file_idx]
             .chunk_by_mut(|a, b| a == b)
-            .find(|chunk| chunk[0].is_none() && chunk.len() >= file_size);
+            .find(|chunk| chunk[0] == FREE && chunk.len() >= file_size);
         if let Some(free_chunk) = free_chunk {
             #[allow(clippy::needless_range_loop)]
             for i in 0..file_size {
-                free_chunk[i] = Some(file_id);
+                free_chunk[i] = file_id;
             }
             for i in 0..file_size {
-                blocks[file_idx + i] = None;
+                blocks[file_idx + i] = FREE;
             }
         }
         file_id -= 1;
@@ -58,11 +60,11 @@ fn compact_whole_files(mut blocks: Vec<Option<usize>>) -> usize {
     checksum(&blocks)
 }
 
-fn checksum(blocks: &[Option<usize>]) -> usize {
-    blocks
-        .iter()
-        .enumerate()
-        .map(|(i, b)| i * b.unwrap_or(0))
+fn checksum(blocks: &[i32]) -> u64 {
+    (0..)
+        .zip(blocks)
+        .filter(|&(_, &b)| b != FREE)
+        .map(|(i, &b)| i * b as u64)
         .sum()
 }
 
