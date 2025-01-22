3d84f06054c3054fc60e544115fb1336a11c54c0
Demian Ferreiro <epidemian@gmail.com>
2025-01-22 01:14:59 -0300

    Day 10: simplify code and improve runtime :)

--- src/day_10_hoof_it.rs
+++ src/day_10_hoof_it.rs
@@ -1,49 +1,38 @@
 use anyhow::Context;
-use itertools::iproduct;
+use itertools::{iproduct, Itertools};
 use pathfinding::prelude::{bfs_reach, count_paths};
 
 pub fn run(input: &str) -> aoc::Result<String> {
     let (ref map, w, h) = aoc::parse_grid(input, |ch| ch.to_digit(10).context("Invalid number"))?;
-    let trailheads: Vec<_> = iproduct!(0..w, 0..h)
+    let trailheads = iproduct!(0..w, 0..h)
         .filter(|&(x, y)| map[y][x] == 0)
-        .collect();
+        .collect_vec();
 
     let successors = |&(x, y): &(usize, usize)| {
         let curr_height = map[y][x];
         [(-1, 0), (1, 0), (0, -1), (0, 1)]
             .into_iter()
-            .map(move |(dx, dy)| (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy)))
-            .filter_map(move |(nx, ny)| {
+            .filter_map(move |(dx, dy)| {
+                let (nx, ny) = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
                 let neighbor_height = *map.get(ny)?.get(nx)?;
-                if neighbor_height == curr_height + 1 {
-                    Some((nx, ny))
-                } else {
-                    None
-                }
+                (neighbor_height == curr_height + 1).then_some((nx, ny))
             })
     };
 
-    let score_sum: usize = trailheads
-        .iter()
-        .map(|&trailhead| {
-            bfs_reach(trailhead, successors)
-                .filter(|&(x, y)| map[y][x] == 9)
-                .count()
-        })
-        .sum();
-
-    let trailends: Vec<_> = iproduct!(0..w, 0..h)
-        .filter(|&(x, y)| map[y][x] == 9)
-        .collect();
-    let rating_sum: usize = trailheads
-        .iter()
-        .map(|&trailhead| {
-            trailends
-                .iter()
-                .map(|&trailend| count_paths(trailhead, successors, |&p| p == trailend))
-                .sum::<usize>()
-        })
-        .sum();
+    let mut score_sum = 0;
+    let mut rating_sum = 0;
+    for trailhead in trailheads {
+        let ends = bfs_reach(trailhead, successors)
+            .filter(|&(x, y)| map[y][x] == 9)
+            .collect_vec();
+        let score = ends.len();
+        let rating: usize = ends
+            .iter()
+            .map(|end| count_paths(trailhead, successors, |p| p == end))
+            .sum();
+        score_sum += score;
+        rating_sum += rating;
+    }
 
     Ok(format!("{score_sum} {rating_sum}"))
 }
