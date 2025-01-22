b034e3336ece8e00131bbbca775cde61b4bded0b
Demian Ferreiro <epidemian@gmail.com>
2025-01-22 14:35:02 -0300

    Day 11: simplify and optimize code a little bit
    Split numbers by doing direct arithmetic instead of converting them to
    strings and then re-parsing numbers.

--- src/day_11_plutonian_pebbles.rs
+++ src/day_11_plutonian_pebbles.rs
@@ -10,29 +10,42 @@ pub fn run(input: &str) -> aoc::Result<String> {
 fn count_stones(stones: &[u64], blink_count: u64) -> u64 {
     let mut stone_counts: HashMap<u64, u64> = stones.iter().map(|&s| (s, 1)).collect();
     for _ in 0..blink_count {
-        let mut new_stone_counts = HashMap::new();
-        let mut add_stones = |stone, count| *new_stone_counts.entry(stone).or_insert(0) += count;
+        let mut new_counts = HashMap::new();
         for (&stone, &count) in stone_counts.iter() {
+            let mut add_stones = |stone| *new_counts.entry(stone).or_insert(0) += count;
             if stone == 0 {
-                add_stones(1, count);
+                add_stones(1);
+            } else if let Some((l, r)) = split_digits(stone) {
+                add_stones(l);
+                add_stones(r);
             } else {
-                let stone_str = stone.to_string();
-                if stone_str.len() % 2 == 0 {
-                    let (l, r) = stone_str.split_at(stone_str.len() / 2);
-                    let (l, r) = (l.parse().unwrap(), r.parse().unwrap());
-                    add_stones(l, count);
-                    add_stones(r, count);
-                } else {
-                    add_stones(stone * 2024, count);
-                }
+                add_stones(stone * 2024);
             }
         }
-        stone_counts = new_stone_counts;
+        stone_counts = new_counts;
     }
     stone_counts.into_values().sum()
+}
+
+fn split_digits(n: u64) -> Option<(u64, u64)> {
+    let digits = n.checked_ilog10()? + 1;
+    if digits % 2 != 0 {
+        return None;
+    }
+    let d = 10u64.pow(digits / 2);
+    Some((n / d, n % d))
 }
 
 #[test]
 fn sample_test() {
     assert_eq!(count_stones(&[125, 17], 25), 55312)
+}
+
+#[test]
+fn split_digits_test() {
+    assert_eq!(split_digits(0), None);
+    assert_eq!(split_digits(5), None);
+    assert_eq!(split_digits(123), None);
+    assert_eq!(split_digits(42), Some((4, 2)));
+    assert_eq!(split_digits(4321), Some((43, 21)));
 }
