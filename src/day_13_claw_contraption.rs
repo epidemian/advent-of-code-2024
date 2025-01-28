fef7f0d05905e68cd23dfa8541ac405b3043d065
Demian Ferreiro <epidemian@gmail.com>
2025-01-27 23:16:24 -0300

    Day 13: use integers instead of floating-point arithmetic

--- src/day_13_claw_contraption.rs
+++ src/day_13_claw_contraption.rs
@@ -4,18 +4,16 @@ pub fn run(input: &str) -> aoc::Answer {
     let claw_machines: Vec<_> = input.split("\n\n").map(parse_claw_machine).try_collect()?;
     let p1_total_tokens: u64 = claw_machines
         .iter()
-        .filter_map(|&(a, b, p)| min_tokens(a, b, p))
+        .map(|&(a, b, p)| min_tokens(a, b, p))
         .sum();
     let p2_total_tokens: u64 = claw_machines
         .iter()
-        .filter_map(|&(a, b, (px, py))| {
-            min_tokens(a, b, (px + 10000000000000.0, py + 10000000000000.0))
-        })
+        .map(|&(a, b, (px, py))| min_tokens(a, b, (px + 10000000000000, py + 10000000000000)))
         .sum();
     aoc::answer(p1_total_tokens, p2_total_tokens)
 }
 
-type Point = (f64, f64);
+type Point = (i64, i64);
 
 fn parse_claw_machine(s: &str) -> aoc::Result<(Point, Point, Point)> {
     let numbers = aoc::parse_numbers(s)?;
@@ -23,22 +21,24 @@ fn parse_claw_machine(s: &str) -> aoc::Result<(Point, Point, Point)> {
     Ok(((ax, ay), (bx, by), (price_x, price_y)))
 }
 
-fn min_tokens((ax, ay): Point, (bx, by): Point, (px, py): Point) -> Option<u64> {
+fn min_tokens((ax, ay): Point, (bx, by): Point, (px, py): Point) -> u64 {
     // We want to find the number of button presses, `a` and `b`, solving these equations:
     // a*ax + b*bx = px
     // a*ay + b*by = py
     // So:
-    // a = (px - b*bx)/ax
-    // b = (py - px*ay/ax) / (by - bx*ay/ax)
-    let b = (py - px * ay / ax) / (by - bx * ay / ax);
-    let a = (px - b * bx) / ax;
-    // Since we used floating-point math, the results are approximate.
-    Some(close_to_integer(a)? * 3 + close_to_integer(b)?)
+    // a = (px - b * bx) / ax
+    // b = (py * ax - px * ay) / (by * ax - bx * ay)
+    let (b, b_rem) = div_mod(py * ax - px * ay, by * ax - bx * ay);
+    let (a, a_rem) = div_mod(px - b * bx, ax);
+    // Key presses must be integers. Ignore non-integer solutions.
+    if b_rem != 0 || a_rem != 0 {
+        return 0;
+    }
+    a as u64 * 3 + b as u64
 }
 
-fn close_to_integer(a: f64) -> Option<u64> {
-    let close_enough = (a - a.round()).abs() < 0.001;
-    (close_enough).then(|| a.round() as u64)
+fn div_mod(a: i64, b: i64) -> (i64, i64) {
+    (a / b, a % b)
 }
 
 #[test]
