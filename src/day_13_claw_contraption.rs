e53b67fb76da6fec8cf6e35c604b84125ab86849
Demian Ferreiro <epidemian@gmail.com>
2025-01-27 22:56:35 -0300

    Add day 13 part 2 solution
    This uses floating-point numbers, which i'm not too happy about. But it
    gets the job done :)

--- src/day_13_claw_contraption.rs
+++ src/day_13_claw_contraption.rs
@@ -1,12 +1,21 @@
-use itertools::{iproduct, Itertools};
+use itertools::Itertools;
 
 pub fn run(input: &str) -> aoc::Answer {
     let claw_machines: Vec<_> = input.split("\n\n").map(parse_claw_machine).try_collect()?;
-    let total_tokens: u64 = claw_machines.iter().filter_map(min_tokens).sum();
-    aoc::answer(total_tokens, "")
+    let p1_total_tokens: u64 = claw_machines
+        .iter()
+        .filter_map(|&(a, b, p)| min_tokens(a, b, p))
+        .sum();
+    let p2_total_tokens: u64 = claw_machines
+        .iter()
+        .filter_map(|&(a, b, (px, py))| {
+            min_tokens(a, b, (px + 10000000000000.0, py + 10000000000000.0))
+        })
+        .sum();
+    aoc::answer(p1_total_tokens, p2_total_tokens)
 }
 
-type Point = (u64, u64);
+type Point = (f64, f64);
 
 fn parse_claw_machine(s: &str) -> aoc::Result<(Point, Point, Point)> {
     let numbers = aoc::parse_numbers(s)?;
@@ -14,11 +23,22 @@ fn parse_claw_machine(s: &str) -> aoc::Result<(Point, Point, Point)> {
     Ok(((ax, ay), (bx, by), (price_x, price_y)))
 }
 
-fn min_tokens(&((ax, ay), (bx, by), (price_x, price_y)): &(Point, Point, Point)) -> Option<u64> {
-    iproduct!(0..=100, 0..=100)
-        .filter(|(a, b)| (a * ax + b * bx, a * ay + b * by) == (price_x, price_y))
-        .map(|(a, b)| a * 3 + b)
-        .min()
+fn min_tokens((ax, ay): Point, (bx, by): Point, (px, py): Point) -> Option<u64> {
+    // We want to find the number of button presses, `a` and `b`, solving these equations:
+    // a*ax + b*bx = px
+    // a*ay + b*by = py
+    // So:
+    // a = (px - b*bx)/ax
+    // b = (py - px*ay/ax) / (by - bx*ay/ax)
+    let b = (py - px * ay / ax) / (by - bx * ay / ax);
+    let a = (px - b * bx) / ax;
+    // Since we used floating-point math, the results are approximate.
+    Some(close_to_integer(a)? * 3 + close_to_integer(b)?)
+}
+
+fn close_to_integer(a: f64) -> Option<u64> {
+    let close_enough = (a - a.round()).abs() < 0.001;
+    (close_enough).then(|| a.round() as u64)
 }
 
 #[test]
@@ -39,5 +59,5 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
 ";
-    assert_eq!(run(sample).unwrap(), "480 ")
+    assert_eq!(run(sample).unwrap(), "480 875318608908")
 }
