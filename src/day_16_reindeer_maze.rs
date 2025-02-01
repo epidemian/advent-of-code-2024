b0eb51d0305a2da1d3d108f55cd696c3fc9556ce
Demian Ferreiro <epidemian@gmail.com>
2025-02-01 16:48:09 -0300

    Day 16: add horribly slow solution for part 2 :)

--- src/day_16_reindeer_maze.rs
+++ src/day_16_reindeer_maze.rs
@@ -1,30 +1,47 @@
 use anyhow::Context;
-use itertools::iproduct;
-use pathfinding::directed::dijkstra::dijkstra;
+use itertools::{iproduct, Itertools};
+use pathfinding::{directed::dijkstra::dijkstra, prelude::dijkstra_reach};
 
 pub fn run(input: &str) -> aoc::Answer {
     let (maze, w, h) = aoc::parse_char_grid(input)?;
     let start_pos = iproduct!(0..w, 0..h)
         .find(|&(x, y)| maze[y][x] == 'S')
         .context("Start not found")?;
-    let start_dir = (1, 0);
-    let (_, best_score) = dijkstra(
-        &(start_pos, start_dir),
-        |&((x, y), (dx, dy))| {
-            let forwards = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
-            [
-                ((forwards, (dx, dy)), 1),   // Advance
-                (((x, y), (-dy, dx)), 1000), // Rotate right
-                (((x, y), (dy, -dx)), 1000), // Rotate left
-            ]
-            .into_iter()
-            .filter(|&(((x, y), _), _)| maze[y][x] != '#')
-        },
-        |&((x, y), _)| maze[y][x] == 'E',
-    )
-    .context("Path to end not found")?;
+    let start = (start_pos, (1, 0));
+    let successors = |&((x, y), (dx, dy)): &((usize, usize), (isize, isize))| {
+        let forwards = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
+        [
+            ((forwards, (dx, dy)), 1),   // Advance
+            (((x, y), (-dy, dx)), 1000), // Rotate right
+            (((x, y), (dy, -dx)), 1000), // Rotate left
+        ]
+        .into_iter()
+        .filter(|&(((x, y), _), _)| maze[y][x] != '#')
+    };
+    let (_, best_score) = dijkstra(&start, successors, |&((x, y), _)| maze[y][x] == 'E')
+        .context("Path to end not found")?;
 
-    aoc::answer(best_score)
+    let possible_tiles: Vec<_> = dijkstra_reach(&start, |node, _cost| successors(node))
+        .take_while(|node| node.total_cost <= best_score)
+        .collect();
+
+    let mut count = 0;
+    let total = possible_tiles.len();
+    let best_path_tile_count = possible_tiles
+        .into_iter()
+        .filter(|node| {
+            let (_, cost) =
+                dijkstra(&node.node, successors, |&((x, y), _)| maze[y][x] == 'E').unwrap();
+            count += 1;
+            if count % 100 == 0 {
+                eprintln!("{count}/{total} processed");
+            }
+            cost + node.total_cost == best_score
+        })
+        .map(|node| node.node.0)
+        .unique()
+        .count();
+    aoc::answers(best_score, best_path_tile_count)
 }
 
 #[test]
@@ -45,5 +62,5 @@ fn sample_test() {
 #S..#.....#...#
 ###############
 ";
-    assert_eq!(run(sample).unwrap(), "7036")
+    assert_eq!(run(sample).unwrap(), "7036 45")
 }
