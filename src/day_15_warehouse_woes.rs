c9289d61e61a4121764f76acc0ef597dbf3524bf
Demian Ferreiro <epidemian@gmail.com>
2025-02-01 00:56:34 -0300

    Day 15: Use a HashMap instead of a 2D grid
    Gain some code clarity at the expense of some extra hashing.
    
    This also makes the solution more correct. It now gives an answer in
    case of a bad input, instead of panicking for accessing a vector out of
    bounds.

--- src/day_15_warehouse_woes.rs
+++ src/day_15_warehouse_woes.rs
@@ -1,36 +1,42 @@
 use anyhow::Context;
 use itertools::iproduct;
 use pathfinding::prelude::bfs_reach;
+use rustc_hash::FxHashMap as HashMap;
 
 pub fn run(input: &str) -> aoc::Answer {
     let (map, instructions) = input.split_once("\n\n").context("Invalid input")?;
     let (map, w, h) = aoc::parse_char_grid(map)?;
+    let map = iproduct!(0..w, 0..h)
+        .map(|(x, y)| ((x as isize, y as isize), map[y][x]))
+        .filter(|&(_, ch)| ch != '.')
+        .collect();
     let wide_map = widen_map(&map);
     aoc::answers(
-        run_robot(map, instructions, w, h)?,
-        run_robot(wide_map, instructions, w * 2, h)?,
+        run_robot(map, instructions)?,
+        run_robot(wide_map, instructions)?,
     )
 }
 
-fn widen_map(map: &[Vec<char>]) -> Vec<Vec<char>> {
-    let widen_char = |&ch| match ch {
-        'O' => ['[', ']'],
-        '@' => ['@', '.'],
-        _ => [ch, ch],
+type Map = HashMap<(isize, isize), char>;
+
+fn widen_map(map: &Map) -> Map {
+    let widen_thing = |(&(x, y), &ch)| {
+        let new_things = match ch {
+            'O' => vec!['[', ']'],
+            '@' => vec!['@'],
+            _ => vec![ch, ch],
+        };
+        (0..)
+            .zip(new_things)
+            .map(move |(i, thing)| ((x * 2 + i, y), thing))
     };
-    map.iter()
-        .map(|row| row.iter().flat_map(widen_char).collect())
-        .collect()
+    map.iter().flat_map(widen_thing).collect()
 }
 
-fn run_robot(
-    mut map: Vec<Vec<char>>,
-    instructions: &str,
-    width: usize,
-    height: usize,
-) -> aoc::Result<usize> {
-    let (mut x, mut y) = iproduct!(0..width, 0..height)
-        .find(|&(x, y)| map[y][x] == '@')
+fn run_robot(mut map: Map, instructions: &str) -> aoc::Result<isize> {
+    let (&(mut x, mut y), _) = map
+        .iter()
+        .find(|&(_, &ch)| ch == '@')
         .context("Robot not found")?;
     for ins in instructions.chars() {
         let (dx, dy) = match ins {
@@ -40,38 +46,31 @@ fn run_robot(
             '<' => (-1, 0),
             _ => continue,
         };
-        let things_to_move: Vec<_> = bfs_reach((x, y), |&pos| {
-            let (x2, y2) = add_signed(pos, (dx, dy));
-            let ch = map.get(y2).and_then(|r| r.get(x2));
-            match ch {
-                None | Some('.') => vec![],
+        let things_to_move: Vec<_> = bfs_reach((x, y), |&(x, y)| {
+            let (x2, y2) = (x + dx, y + dy);
+            match map.get(&(x2, y2)) {
+                None => vec![],
                 Some('[') => vec![(x2, y2), (x2 + 1, y2)],
                 Some(']') => vec![(x2 - 1, y2), (x2, y2)],
                 Some(_) => vec![(x2, y2)],
             }
         })
-        .map(|(x, y)| (map[y][x], x, y))
+        .map(|pos| (map[&pos], pos))
         .collect();
         let pushing_wall = things_to_move.iter().any(|&(ch, ..)| ch == '#');
         if !pushing_wall {
-            for &(_, x, y) in &things_to_move {
-                map[y][x] = '.';
+            for &(_, pos) in &things_to_move {
+                map.remove(&pos);
             }
-            for &(ch, x, y) in &things_to_move {
-                let (new_x, new_y) = add_signed((x, y), (dx, dy));
-                map[new_y][new_x] = ch;
+            for &(ch, (x, y)) in &things_to_move {
+                let (new_x, new_y) = (x + dx, y + dy);
+                map.insert((new_x, new_y), ch);
             }
-            (x, y) = add_signed((x, y), (dx, dy));
+            (x, y) = (x + dx, y + dy);
         };
     }
-    let box_coords = iproduct!(0..width, 0..height)
-        .filter(|&(x, y)| map[y][x] == 'O' || map[y][x] == '[')
-        .map(|(x, y)| y * 100 + x);
-    Ok(box_coords.sum())
-}
-
-fn add_signed((x, y): (usize, usize), (dx, dy): (isize, isize)) -> (usize, usize) {
-    (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy))
+    let boxes = map.iter().filter(|&(_, &ch)| ch == 'O' || ch == '[');
+    Ok(boxes.map(|((x, y), _)| y * 100 + x).sum())
 }
 
 #[test]
@@ -99,4 +98,16 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 ";
     assert_eq!(run(sample).unwrap(), "10092 9021")
+}
+
+#[test]
+fn bad_map_test() {
+    let input = "#####
+#...#
+#.@O.
+#...#
+#####
+
+>>>>>";
+    assert!(run(input).is_ok())
 }
