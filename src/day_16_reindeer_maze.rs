a8caec1460095c712f37cb240484f9d6b18319f6
Demian Ferreiro <epidemian@gmail.com>
2025-02-08 17:29:11 -0300

    Day 16: use a single rotation number instead of 2D direction

--- src/day_16_reindeer_maze.rs
+++ src/day_16_reindeer_maze.rs
@@ -5,22 +5,21 @@ use rustc_hash::FxHashSet as HashSet;
 
 pub fn run(input: &str) -> aoc::Answer {
     let (maze, w, h) = aoc::parse_char_grid(input)?;
-    let start_pos = iproduct!(0..w, 0..h)
+    let (start_x, start_y) = iproduct!(0..w, 0..h)
         .find(|&(x, y)| maze[y][x] == 'S')
         .context("Start not found")?;
-    let start = (start_pos, (1, 0));
-    let successors = |&((x, y), (dx, dy)): &((usize, usize), (isize, isize))| {
-        let forwards = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
-        [
-            ((forwards, (dx, dy)), 1),   // Advance
-            (((x, y), (-dy, dx)), 1000), // Rotate right
-            (((x, y), (dy, -dx)), 1000), // Rotate left
-        ]
-        .into_iter()
-        .filter(|&(((x, y), _), _)| maze[y][x] != '#')
+    let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+    let start = (start_x, start_y, 0);
+    let successors = |&(x, y, d): &(usize, usize, usize)| {
+        let (dx, dy) = dirs[d];
+        let forwards = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy), d);
+        let rot_r = (x, y, (d + 1) % 4);
+        let rot_l = (x, y, (d + 3) % 4);
+        [(forwards, 1), (rot_r, 1000), (rot_l, 1000)]
+            .into_iter()
+            .filter(|&((x, y, _), _)| maze[y][x] != '#')
     };
-
-    let (parents, end) = dijkstra_partial(&start, successors, |&((x, y), _)| maze[y][x] == 'E');
+    let (parents, end) = dijkstra_partial(&start, successors, |&(x, y, _)| maze[y][x] == 'E');
     let end = end.context("Path to end not found")?;
     let best_score = parents[&end].1;
 
@@ -47,8 +46,8 @@ pub fn run(input: &str) -> aoc::Answer {
             break;
         }
     }
+    let best_paths_tiles: HashSet<_> = best_paths_nodes.iter().map(|&(x, y, ..)| (x, y)).collect();
 
-    let best_paths_tiles: HashSet<_> = best_paths_nodes.into_iter().map(|(pos, _)| pos).collect();
     aoc::answers(best_score, best_paths_tiles.len())
 }
 
