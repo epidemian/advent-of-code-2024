05303c382ac5043879ec2dc0f0b22137ca5561bc
Demian Ferreiro <epidemian@gmail.com>
2025-02-11 20:23:37 -0300

    Day 17: sketch out part 2
    This brute-force approach works for the sample input, but not for the
    actual input.

--- src/day_17_chronospatial_computer.rs
+++ src/day_17_chronospatial_computer.rs
@@ -1,8 +1,16 @@
-use anyhow::{bail, Context};
+use anyhow::Context;
 use itertools::Itertools;
 
 pub fn run(input: &str) -> aoc::Answer {
-    let (mut a, mut b, mut c, program) = parse_program(input)?;
+    let (a, b, c, program) = parse_program(input)?;
+    let p1_ans = run_program(a, b, c, &program).iter().join(",");
+    let p2_ans = (0..1_000_000)
+        .find(|&a| run_program(a, b, c, &program) == program)
+        .context("Could not find a value for A")?;
+    aoc::answers(p1_ans, p2_ans)
+}
+
+fn run_program(mut a: u64, mut b: u64, mut c: u64, program: &[u64]) -> Vec<u64> {
     let mut ip = 0;
     let mut outputs = vec![];
     while ip < program.len() {
@@ -29,13 +37,12 @@ pub fn run(input: &str) -> aoc::Answer {
             5 => outputs.push(combo_op() % 8),
             7 => c = a >> combo_op(),
             _ => {
-                bail!("Invalid opcode {opcode}")
+                unreachable!("Invalid opcode {opcode}")
             }
         }
         ip += 2;
     }
-
-    aoc::answer(outputs.iter().join(","))
+    outputs
 }
 
 fn parse_program(input: &str) -> aoc::Result<(u64, u64, u64, Vec<u64>)> {
@@ -47,12 +54,24 @@ fn parse_program(input: &str) -> aoc::Result<(u64, u64, u64, Vec<u64>)> {
 }
 
 #[test]
-fn sample_test() {
+fn part_1_sample_test() {
     let sample = "Register A: 729
 Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0
 ";
-    assert_eq!(run(sample).unwrap(), "4,6,3,5,6,3,5,2,1,0")
+    let (a, b, c, prog) = parse_program(sample).unwrap();
+    assert_eq!(run_program(a, b, c, &prog), [4, 6, 3, 5, 6, 3, 5, 2, 1, 0])
+}
+
+#[test]
+fn part_2_sample_test() {
+    let sample = "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+";
+    assert_eq!(run(sample).unwrap(), "5,7,3,0 117440")
 }
