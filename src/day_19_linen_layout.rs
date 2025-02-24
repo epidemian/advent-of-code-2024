2055fdcb7d5cd38e1ecc170d5a3da2e8e5e5ab31
Demian Ferreiro <epidemian@gmail.com>
2025-02-24 00:29:34 -0300

    Add day 19 part 2 solution

--- src/day_19_linen_layout.rs
+++ src/day_19_linen_layout.rs
@@ -1,13 +1,44 @@
 use anyhow::Context;
 use itertools::Itertools;
-use regex::Regex;
+use rustc_hash::FxHashMap as HashMap;
 
 pub fn run(input: &str) -> aoc::Answer {
     let (towels, designs) = input.split_once("\n\n").context("Invalid input")?;
-    let re_pattern = format!("^({})*$", towels.split(", ").join("|"));
-    let re = Regex::new(&re_pattern).unwrap();
-    let possible_designs_count = designs.lines().filter(|d| re.is_match(d)).count();
-    aoc::answer(possible_designs_count)
+    let towels = towels.split(", ").collect_vec();
+    let designs = designs.lines().collect_vec();
+    let mut cache = HashMap::default();
+    let arrangement_counts = designs
+        .into_iter()
+        .map(|design| count_arrangements(design, &towels, &mut cache))
+        .collect_vec();
+    aoc::answers(
+        arrangement_counts.iter().filter(|&&c| c > 0).count(),
+        arrangement_counts.iter().sum::<u64>(),
+    )
+}
+
+fn count_arrangements<'a>(
+    design: &'a str,
+    towels: &[&str],
+    cache: &mut HashMap<&'a str, u64>,
+) -> u64 {
+    if design.is_empty() {
+        return 1;
+    }
+    if let Some(&n) = cache.get(design) {
+        return n;
+    }
+    let count = towels
+        .iter()
+        .map(|t| {
+            let Some(remaining_design) = design.strip_prefix(t) else {
+                return 0;
+            };
+            count_arrangements(remaining_design, towels, cache)
+        })
+        .sum();
+    cache.insert(design, count);
+    count
 }
 
 #[test]
@@ -23,5 +54,5 @@ bwurrg
 brgr
 bbrgwb
 ";
-    assert_eq!(run(sample).unwrap(), "6")
+    assert_eq!(run(sample).unwrap(), "6 16")
 }
