00ee760b2dbf6eda73d9328bfc1d579390369854
Demian Ferreiro <epidemian@gmail.com>
2025-03-21 03:22:44 -0300

    Day 21: add solution for part 2
    Once again caching saved the day! Oof, i'm tired after this one.

--- src/day_21_keypad_conundrum.rs
+++ src/day_21_keypad_conundrum.rs
@@ -1,8 +1,11 @@
-use itertools::Itertools;
 use rustc_hash::FxHashMap as HashMap;
 
 pub fn run(input: &str) -> aoc::Answer {
-    let numeric_keypad = &HashMap::from_iter([
+    aoc::answers(run_robots_chain(input, 2), run_robots_chain(input, 25))
+}
+
+fn run_robots_chain(input: &str, robot_count: u32) -> usize {
+    let numeric_keypad = HashMap::from_iter([
         ('7', (0, 0)),
         ('8', (1, 0)),
         ('9', (2, 0)),
@@ -16,7 +19,7 @@ pub fn run(input: &str) -> aoc::Answer {
         ('0', (1, 3)),
         ('A', (2, 3)),
     ]);
-    let directional_keypad = &HashMap::from_iter([
+    let directional_keypad = HashMap::from_iter([
         (' ', (0, 0)),
         ('^', (1, 0)),
         ('A', (2, 0)),
@@ -24,54 +27,48 @@ pub fn run(input: &str) -> aoc::Answer {
         ('v', (1, 1)),
         ('>', (2, 1)),
     ]);
-    let p1_keypad_chain = [numeric_keypad, directional_keypad, directional_keypad];
-    let mut p2_keypad_chain = vec![numeric_keypad];
-    p2_keypad_chain.extend(vec![directional_keypad; 24]);
-
-    // aoc::answers(
-    //     run_keypad_chain(input, &p1_keypad_chain),
-    //     run_keypad_chain(input, &p2_keypad_chain),
-    // )
-    aoc::answer(run_keypad_chain(input, &p1_keypad_chain))
+    let mut cache = HashMap::default();
+    let code_complexities = input.lines().map(|code| {
+        let min_seq_len = calc_button_presses(code, &numeric_keypad)
+            .iter()
+            .map(|sequence| {
+                get_shortest_final_sequence(sequence, &directional_keypad, robot_count, &mut cache)
+            })
+            .min()
+            .unwrap();
+        let code_num = code[0..3].parse::<usize>().unwrap();
+        min_seq_len * code_num
+    });
+    code_complexities.sum()
 }
 
-fn run_keypad_chain(input: &str, keypad_chain: &[&HashMap<char, (i32, i32)>]) -> u64 {
-    let code_complexities_sum: u64 = input
-        .lines()
-        .map(|code| {
-            // println!("processing {code}");
-            let sequences = keypad_chain
-                .iter()
-                .fold(vec![code.to_string()], |seqs, keypad| {
-                    let mut next_sequences = seqs
-                        .into_iter()
-                        .flat_map(|seq| get_button_presses(&seq, keypad))
-                        .collect_vec();
-                    let min_seq_len = next_sequences
-                        .iter()
-                        .map(|seq| seq.len())
-                        .min()
-                        .unwrap_or(0);
-                    next_sequences.retain(|seq| seq.len() == min_seq_len);
-                    // println!("#seqs: {}", next_sequences.len());
-                    // println!("min seq: {min_seq_len}");
-
-                    next_sequences
-                });
-            // println!();
-            let min_sequence_len = sequences
-                .iter()
-                .map(|seq| seq.len() as u64)
-                .min()
-                .unwrap_or(0);
-            let code_num = code[0..3].parse::<u64>().unwrap();
-            min_sequence_len * code_num
-        })
-        .sum();
-    code_complexities_sum
+fn get_shortest_final_sequence(
+    sequence: &str,
+    keypad: &HashMap<char, (i32, i32)>,
+    robot_depth: u32,
+    cache: &mut HashMap<(String, u32), usize>,
+) -> usize {
+    if robot_depth == 0 {
+        return sequence.len();
+    }
+    if let Some(&min_len) = cache.get(&(sequence.to_string(), robot_depth)) {
+        return min_len;
+    }
+    let mut total_len = 0;
+    for sub_seq in sequence.split_inclusive('A') {
+        total_len += calc_button_presses(sub_seq, keypad)
+            .into_iter()
+            .map(|next_robot_seq| {
+                get_shortest_final_sequence(&next_robot_seq, keypad, robot_depth - 1, cache)
+            })
+            .min()
+            .unwrap();
+    }
+    cache.insert((sequence.to_string(), robot_depth), total_len);
+    total_len
 }
 
-fn get_button_presses(seq: &str, keypad: &HashMap<char, (i32, i32)>) -> Vec<String> {
+fn calc_button_presses(seq: &str, keypad: &HashMap<char, (i32, i32)>) -> Vec<String> {
     let (mut x, mut y) = keypad[&'A'];
     let mut button_presses = vec![String::new()];
     let (bad_x, bad_y) = keypad[&' '];
@@ -111,5 +108,5 @@ fn sample_test() {
 456A
 379A
 ";
-    assert_eq!(run(sample).unwrap(), "126384")
+    assert_eq!(run_robots_chain(sample, 2), 126384)
 }
