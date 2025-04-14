54c9e53c6e1d22cbf7439bb8d6d856efadc6e92a
Demian Ferreiro <epidemian@gmail.com>
2025-04-14 04:07:34 -0300

    Day 22: use imperative code instead of iterators

--- src/day_22_monkey_market.rs
+++ src/day_22_monkey_market.rs
@@ -1,50 +1,29 @@
-use itertools::{Itertools, zip_eq};
 use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 
 pub fn run(input: &str) -> aoc::Answer {
-    let initial_secrets = aoc::parse_numbers(input)?;
-    let secrets: Vec<Vec<u64>> = initial_secrets
-        .into_iter()
-        .map(|secret| {
-            (0..=2000)
-                .scan(secret, |s, _| {
-                    let res = Some(*s);
-                    *s = rand(*s);
-                    res
-                })
-                .collect()
-        })
-        .collect();
-    let prices: Vec<Vec<u8>> = secrets
-        .iter()
-        .map(|monkey_secrets| monkey_secrets.iter().map(|s| (s % 10) as u8).collect())
-        .collect();
-    let diffs: Vec<Vec<u8>> = prices
-        .iter()
-        .map(|monkey_prices| {
-            monkey_prices
-                .iter()
-                .tuple_windows()
-                .map(|(&a, &b)| 10 + b - a) // Avoid negative diff
-                .collect()
-        })
-        .collect();
+    let secrets = aoc::parse_numbers(input)?;
+    let mut secrets_sum = 0_u64;
     let mut total_bananas = HashMap::default();
-    for (monkey_diffs, monkey_prices) in zip_eq(diffs, prices) {
+    for mut secret in secrets {
+        // Store 4 price diffs as 4 bytes in a u32. Using a [u8; 4] is slower for some reason.
+        let mut diffs = 0_u32;
         let mut diffs_seen = HashSet::default();
-        for (diffs, &price) in zip_eq(monkey_diffs.windows(4), &monkey_prices[4..]) {
-            let diffs: [u8; 4] = diffs.try_into().unwrap();
-            if diffs_seen.insert(diffs) {
-                *total_bananas.entry(diffs).or_insert(0) += price as u32;
+        for i in 0..2000 {
+            let prev_secret = secret;
+            secret = rand(secret);
+            let diff = 10 + secret % 10 - prev_secret % 10;
+            diffs = diffs << 8 | diff;
+            if i >= 3 && diffs_seen.insert(diffs) {
+                *total_bananas.entry(diffs).or_insert(0) += secret % 10;
             }
         }
+        secrets_sum += secret as u64;
     }
-    let sum: u64 = secrets.iter().flat_map(|s| s.last()).sum();
     let max_bananas = total_bananas.values().max().unwrap_or(&0);
-    aoc::answers(sum, max_bananas)
+    aoc::answers(secrets_sum, max_bananas)
 }
 
-fn rand(mut s: u64) -> u64 {
+fn rand(mut s: u32) -> u32 {
     let mask = 0xFFFFFF;
     s = ((s << 6) ^ s) & mask;
     s = ((s >> 5) ^ s) & mask;
@@ -54,20 +33,12 @@ fn rand(mut s: u64) -> u64 {
 
 #[test]
 fn part_1_sample_test() {
-    let sample = "1
-10
-100
-2024
-";
+    let sample = "1\n10\n100\n2024";
     assert_eq!(run(sample).unwrap(), "37327623 24")
 }
 
 #[test]
 fn part_2_sample_test() {
-    let sample = "1
-2
-3
-2024
-";
+    let sample = "1\n2\n3\n2024";
     assert_eq!(run(sample).unwrap(), "37990510 23")
 }
